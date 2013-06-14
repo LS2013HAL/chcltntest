9924055f805cacbe459c2cf3b90fb4e08343767a
HAL <braveduck_free336@hotmail.co.jp>
2013-06-14 23:32:22 +0900

    Sample.java追加

---
+++ Sample.java
@@ -0,0 +1,27 @@
+package jp.ls.ateam.sample;
+
+/*
+ *  サンプルクラス
+ */
+public class Sample {
+
+/**
+ * メインメソッド
+ *
+ * @param args　
+ * 起動パラメータ
+ *
+ */
+public static void main(String[] args) {
+
+// 挨拶をコンソールに表示する
+System.out.println(getGreeting());
+}
+
+/*
+ * 挨拶取得メソッド
+ */
+public static String getGreeting(){
+return "Hello!!";
+
+}}
